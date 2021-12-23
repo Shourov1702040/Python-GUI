77a4bfe17d16ac75c28b0205f5042993facc183b
Md Touhid Islam <36205091+Shourov1702040@users.noreply.github.com>
2021-12-24 02:11:42 +0600

    Create 03_ways_of _using_kivy.py

---
+++ Kivymd/03_ways_of _using_kivy/03_ways_of _using_kivy.py
@@ -0,0 +1,15 @@
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+
+class MyApp(MDApp):
+	def build(self):
+		
+		self.screen = Builder.load_file('D:/Working_dir/Programming/Python/KivyMD/03_ways_of _using_kivy.kv')
+
+		return self.screen
+
+	def button_press(self):
+		print("My buttons pressed")
+
+if __name__=='__main__':
+	MyApp().run()
